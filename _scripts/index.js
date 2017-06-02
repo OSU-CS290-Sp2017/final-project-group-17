9a262e567929d29c692a1acba26a7333eb4917ac
jimweldev <jimwela@Jimwels-MacBook-Pro.local>
2017-06-02 02:33:05 -0700

    added hover interactions with UIsocialbuttons

--- _scripts/index.js
+++ _scripts/index.js
@@ -1,8 +1,30 @@
 var loginButton = document.getElementById('loginButton');
 var modal = document.getElementById('login-modal');
 var modalBackdrop = document.getElementById('modal-backdrop');
+var closeLoginModal = document.querySelector('.modal-close-button');
+
+var modalHeader = document.querySelector('.login-modal-header');
+var socialButtons = document.getElementsByClassName('UISocialButton');
 
 loginButton.addEventListener('click', function() {
+   modalHeader.innerHTML = 'Login';
+   socialButtons[0].textContent = 'Login with Facebook';
+   socialButtons[1].textContent = 'Login with Google';
+   modal.className = '';
+   modalBackdrop.className = '';
+});
+
+var signupButton = document.getElementById('signupButton');
+
+signupButton.addEventListener('click', function() {
+   modalHeader.innerHTML = 'Sign up';
+   socialButtons[0].textContent = 'Sign up with Facebook';
+   socialButtons[1].textContent = 'Sign up with Google';
     modal.className = '';
     modalBackdrop.className = '';
 });
+
+closeLoginModal.addEventListener('click', function() {
+   modal.className = 'hidden';
+   modalBackdrop.className = 'hidden';
+})
