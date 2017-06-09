bf3599b372d826f8bf0939da978bbf0fbd165be5
jimweldev <aguinaji@oregonstate.edu>
2017-06-09 02:54:33 -0700

    no sets message

--- _scripts/create.js
+++ _scripts/create.js
@@ -18,6 +18,7 @@ closeSetModal.addEventListener('click', function(){
 var createModal = document.querySelector('.modal-create-button');
 var modalInputElement = document.querySelector('.modal-input-element');
 var modalInputText = document.getElementById('setclass-input');
+var statusSets = document.getElementById('status');
 
 createModal.addEventListener('click', function() {
   if (modalInputText.value === "") {
@@ -26,17 +27,26 @@ createModal.addEventListener('click', function() {
   }
   else {
     var mainContainer = document.querySelector('.mainContainer');
+    var studyCardLink = document.createElement('a');
+    studyCardLink.setAttribute('href', 'card.html');
+    studyCardLink.classList.add('studycardlink');
     var setContainer = document.createElement('div');
     setContainer.classList.add('setContainer');
     var setClassName = document.createElement('p');
     var classNameTxt = document.createTextNode(modalInputText.value);
 
-    mainContainer.appendChild(setContainer);
+    mainContainer.appendChild(studyCardLink);
+    studyCardLink.appendChild(setContainer);
     setContainer.appendChild(setClassName);
     setClassName.appendChild(classNameTxt);
 
     addSetmodal.className = 'hidden';
     modalBackdrop.className = 'hidden';
     modalInputText.value = '';
+
+    statusSets.className = 'hidden';
+    if(statusSets.className == ''){
+      className = 'hidden';
+   }
   }
 });
