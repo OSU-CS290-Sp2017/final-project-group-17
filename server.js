4257552b18ee2db19804ab386881fa6633faf53b
Carlos Lopez <Carlos231@users.noreply.github.com>
2017-06-12 22:00:05 -0700

    added error page and console.log name

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var path = require('path');
 var fs = require('fs');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
 
 var bodyParser = require('body-parser');
 
@@ -21,68 +22,35 @@ app.get('/', function (req, res, next) {
 });
 
 app.get('/classes', function (req, res, next) {
-
+  console.log("== url params for request:", req.params);
   var templateArgs = {
     classes: classData,
-    title: "Class name - " + classData.name,
     stylesheet2: "/create.css",
-    script2: "/create.js"
+    script2: "/create.js",
   };
 
   res.render('classPage', templateArgs);
 
 });
 
-app.get('/classes/:classSet', function (req, res, next) {
-  console.log("== url params for request:", req.params);
-  var classSet = req.params.classSet;
-  var classData = classData[classSet]; //added
-  if (classData){
-    var templateArgs = {
-      //flashCard: classData[classSet].studySet,
-      studySet: classData.studySet,
-      //to save the data in classDat.json
-      name: classData.name,
-      title: classData.name + " class",
-      stylesheet2: "/card.css",
-      script2: "/card.js",
-    }
-  res.render('cardPage', templateArgs);
-  } else {
-      next();
-  }
-});
+app.post('/classes/addclass', function (req, res, next) {
 
-///----------------------------------------------------------------
-app.post('/classes/:classSet/addClass', function (req, res, next) {
-  var classSet = classData[req.params.classSet];
+   var newClassSet = {
+      class: req.body.class,
+      studySet: []
+   };
 
-  if (classSet) {
-    if (req.body && req.body.term) {
+   var key = req.body.key;
+   classData[key] = newClassSet;
+   fs.writeFile('classData.json', JSON.stringify(classData, null, 2), function(err) {
+      if(err) {
+         res.status(500).send('Unable to create new class set');
+      }
+      else {
+         res.status(200).send();
+      }
+   });
 
-      var aClass = {
-        term: req.body.term,
-        definition: req.body.definition
-      };
-
-      classSet.studySet = classSet.studySet || [];
-
-      classSet.studySet.push(aClass);
-      fs.writeFile('classData.json', JSON.stringify(classData), function (err) {
-        if (err) {
-          res.status(500).send("Unable to save photo to \"database\".");
-        } else {
-          res.status(200).send();
-        }
-      });
-
-    } else {
-      res.status(400).send("Person photo must have a URL.");
-    }
-
-  } else {
-    next();
-  }
 });
 ///----------------------------------------------------------------
 
@@ -90,10 +58,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('*', function (req, res) {
   res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
-
-// Start the server listening on the specified port.
 app.listen(port, function () {
   console.log("== Server listening on port", port);
 });
-
-//app.listen(port);
