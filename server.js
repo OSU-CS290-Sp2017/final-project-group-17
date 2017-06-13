05d920d83265121bdd6339dd5ef7b7dfd92a96b2
jimweldev <aguinaji@oregonstate.edu>
2017-06-13 09:25:44 -0700

    modified features

--- server.js
+++ server.js
@@ -88,6 +88,7 @@ app.post('/classes/:studySet/addCard', function (req, res, next) {
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('*', function (req, res) {
   res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
