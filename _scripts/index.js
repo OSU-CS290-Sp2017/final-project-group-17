be4bf1fd29cd45bd6098d88042d6e333297cd5f0
Ryan Wallerius <Ryanwallerius@10-248-184-26.wireless.oregonstate.edu>
2017-06-12 22:19:04 -0700

    made changes to button

---
+++ _scripts/index.js
@@ -0,0 +1,30 @@
+var loginButton = document.getElementById('loginButton');
+var modal = document.getElementById('login-modal');
+var modalBackdrop = document.getElementById('modal-backdrop');
+var closeLoginModal = document.getElementById('closeLoginModal');
+
+var modalHeader = document.getElementById('userModalHeader');
+var socialButtons = document.getElementsByClassName('UISocialButton');
+
+loginButton.addEventListener('click', function() {
+   modalHeader.innerHTML = 'Login';
+   socialButtons[0].textContent = 'Login with Facebook';
+   socialButtons[1].textContent = 'Login with Google';
+   modal.className = '';
+   modalBackdrop.className = '';
+});
+
+var signupButton = document.getElementById('signupButton');
+
+signupButton.addEventListener('click', function() {
+   modalHeader.innerHTML = 'Sign up';
+   socialButtons[0].textContent = 'Sign up with Facebook';
+   socialButtons[1].textContent = 'Sign up with Google';
+    modal.className = '';
+    modalBackdrop.className = '';
+});
+
+closeLoginModal.addEventListener('click', function() {
+   modal.className = 'hidden';
+   modalBackdrop.className = 'hidden';
+})
